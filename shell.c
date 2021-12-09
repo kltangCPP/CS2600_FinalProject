8538250eb082574a422c70e2df7b56806cd2e7a9
kltangCPP <89665170+kltangCPP@users.noreply.github.com>
2021-12-09 00:56:47 -0800

    Fixed typo in comment

--- shell.c
+++ shell.c
@@ -1,12 +1,6 @@
 /***************************************************************************//**
 
-  @file         main.c
-
-  @author       Stephen Brennan
-
-  @date         Thursday,  8 January 2015
-
-  @brief        LSH (Libstephen SHell)
+Followed tutorial by Stephen Brennan
 
 *******************************************************************************/
 
@@ -17,16 +11,10 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-  Function Declarations for builtin shell commands:
- */
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
-/*
-  List of builtin commands, followed by their corresponding functions.
- */
 char *builtin_str[] = {
   "cd",
   "help",
@@ -43,15 +31,6 @@ int lsh_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
-/*
-  Builtin function implementations.
-*/
-
-/**
-   @brief Bultin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
- */
 int lsh_cd(char **args)
 {
   if (args[1] == NULL) {
@@ -64,11 +43,6 @@ int lsh_cd(char **args)
   return 1;
 }
 
-/**
-   @brief Builtin command: print help.
-   @param args List of args.  Not examined.
-   @return Always returns 1, to continue executing.
- */
 int lsh_help(char **args)
 {
   int i;
@@ -84,21 +58,11 @@ int lsh_help(char **args)
   return 1;
 }
 
-/**
-   @brief Builtin command: exit.
-   @param args List of args.  Not examined.
-   @return Always returns 0, to terminate execution.
- */
 int lsh_exit(char **args)
 {
   return 0;
 }
 
-/**
-  @brief Launch a program and wait for it to terminate.
-  @param args Null terminated list of arguments (including program).
-  @return Always returns 1, to continue execution.
- */
 int lsh_launch(char **args)
 {
   pid_t pid;
@@ -124,11 +88,6 @@ int lsh_launch(char **args)
   return 1;
 }
 
-/**
-   @brief Execute shell built-in or launch program.
-   @param args Null terminated list of arguments.
-   @return 1 if the shell should continue running, 0 if it should terminate
- */
 int lsh_execute(char **args)
 {
   int i;
@@ -147,10 +106,6 @@ int lsh_execute(char **args)
   return lsh_launch(args);
 }
 
-/**
-   @brief Read a line of input from stdin.
-   @return The line from stdin.
- */
 char *lsh_read_line(void)
 {
 #ifdef LSH_USE_STD_GETLINE
@@ -206,11 +161,7 @@ char *lsh_read_line(void)
 
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
-/**
-   @brief Split a line into tokens (very naively).
-   @param line The line.
-   @return Null-terminated array of tokens.
- */
+
 char **lsh_split_line(char *line)
 {
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
@@ -244,9 +195,6 @@ char **lsh_split_line(char *line)
   return tokens;
 }
 
-/**
-   @brief Loop getting input and executing it.
- */
 void lsh_loop(void)
 {
   char *line;
@@ -264,12 +212,6 @@ void lsh_loop(void)
   } while (status);
 }
 
-/**
-   @brief Main entry point.
-   @param argc Argument count.
-   @param argv Argument vector.
-   @return status code
- */
 int main(int argc, char **argv)
 {
   // Load config files, if any.
